c84b859a2fcd09713f3281220dd979581e792769
Anees ur Rehman <khananeesu304@gmail.com>
2025-02-10 02:24:16 +0500

    changes done

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoute = require("./routes/user.route");
-const cors = require('cors');
-
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors())
-app.use(userRoute)
+app.use(cors());
+app.use(userRoute);
 
 mongoose
-  .connect("mongodb+srv://khananeesu304:cgEolXCDp6YmTZyr@cluster0.fjpnz.mongodb.net", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://khananeesu304:cgEolXCDp6YmTZyr@cluster0.fjpnz.mongodb.net",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to landingpage API");
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
